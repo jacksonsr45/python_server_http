7ec242aeebcc42a2af3013dc75bd0c709b50d574
jacksonsr45 <31199441+jacksonsr45@users.noreply.github.com>
2020-02-24 19:06:41 -0300

    Create server_py.py
    created file server_py.py

---
+++ server_py.py
@@ -0,0 +1,23 @@
+import os
+try:
+    from SimpleHTTPServer import SimpleHTTPReqquestHandler as SimpleHTTPReqquestHandler
+    from SocketServer import TCPServer as Server
+except ImportError:
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
+
+
+#Read port selected by the cloud for our application
+PORT = int(os.getenv('PORT', 8000))
+
+#Change current directory
+os.chdir('staic')
+
+httpd = Server(("", PORT), Handler)
+
+try:
+    print("Start serving at port %i" % PORT)
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
