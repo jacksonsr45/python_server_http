dfb8f7f8ef5474b552cb038824ed8bb8e206cb8c
jacksonsr45 <31199441+jacksonsr45@users.noreply.github.com>
2020-02-24 23:10:00 -0300

    Add new configuration up class man

---
+++ create_server.py
@@ -0,0 +1,11 @@
+from server.server_py import New_Server
+
+
+class Main:
+    def __init__(self):
+        New_Server()
+
+
+
+if __name__ == "__main__":
+    Main()
--- server/server_py.py
+++ server/server_py.py
@@ -6,29 +6,36 @@ try:
 except ImportError:
     from http.server import SimpleHTTPRequestHandler as Handler
     from http.server import HTTPServer as Server
-
-from config import config as c
-
-
-#Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', c['port']))
-
-#path defoult from BASE_DIR of system
-folder = c['primary_folder']
-path = c['system_name']
-value = os.path.basename(path)
-BASER_DIR = """{}/{}""".format(path, folder)
+from .config import config as c
 
 
-#Change current directory
-os.chdir(BASER_DIR)
-#Exemple https://localhost:8000
-httpd = Server(("%s" %c['local_host'], PORT), Handler)
-try:
-    print("Start serving at port %i" % PORT)
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    httpd.server_close()
+class New_Server:
+    def __init__(self):
+        #Init variables 
+        self.folder = c['primary_folder']
+        self.path = c['system_name']
+                
+        #Read port selected by the cloud for our application
+        PORT = int(os.getenv('PORT', c['port']))
+
+        #Change current directory
+        os.chdir(self.BASE_DIR( self.path, self.folder))     
+        self.UP_SERVER(PORT)
 
 
-httpd.server_close()
+    def BASE_DIR(self, path, folder):
+        #path defoult from BASE_DIR of system
+        value = os.path.basename(path)
+        path_value = '%s' % value+'/'+'%s' % folder
+        return path_value
+
+    def UP_SERVER(self, PORT):
+        #Exemple https://localhost:8000
+        httpd = Server(("%s" %c['local_host'], PORT), Handler)
+        try:
+            print("Start serving at port %i" % PORT)
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            httpd.server_close()
+
+        httpd.server_close()
